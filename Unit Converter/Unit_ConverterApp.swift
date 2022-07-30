4f0bfc08adecfee4d82584a87cb1a179c167bcde
sedatcakir26 <sedatcakir@gmail.com>
2022-07-30 13:05:07 +0300

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Sedat Çakır on 30.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
